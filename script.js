1b4e159d426474d1a170e9b4e172f79923cd56e9
T-Tavares <thiago.tavares.freire@gmail.com>
2024-09-05 15:11:42 +1200

    Steps to flow on git

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Hello, world!');
+
+/* 
+    Step 1 - pull changes from main
+        - middle step stagging branch
+    Step 2 - merge your branch to main
+    Step 3 - push changes to main
+*/
