0bfb6440b7ae0cde67d790a92fbdccc90bdd3e47
Maryanne Galo <maryanneg@missionreadyhq.com>
2024-09-07 19:01:52 +1200

    style heading when btn is clicked

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+console.log("Hello, world!");
+
+const heading = document.querySelector("h1");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  heading.style.backgroundColor = "green";
+});
